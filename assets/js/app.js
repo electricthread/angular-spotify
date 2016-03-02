396523eddfdbbe028268021e009ae45acfa41a35
Evan Willhite <ewillhite@centresource.com>
2016-03-01 21:23:53 -0600

    tweaks per jslint and some testing to fix duplicate issue (still exists)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,25 @@
-(function(){
+(function () {
+  "use strict";
+
   var app = angular.module('spotify', ['song']);
 
-  app.controller('UserController', [ '$http', function($http) {
+  app.controller('UserController', ['$http', function ($http) {
     var user = this,
         mySpotifyUrl = 'https://api.spotify.com/v1/me',
         spotifyUrl = 'https://api.spotify.com/v1/users';
 
     // Login (new window)
-    user.login = function(callback) {
+    user.login = function (callback) {
       var CLIENT_ID = 'ba2e53ebf4ec4ed2acce03ca66c83783',
           REDIRECT_URI = 'http://localhost:9000/callback.html';
-      
+
       function getLoginURL(scopes) {
         return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
           '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
           '&scope=' + encodeURIComponent(scopes.join(' ')) +
           '&response_type=token';
       }
-          
+
       var url = getLoginURL(['playlist-modify-public']);
           
       var width = 450,
@@ -25,7 +27,7 @@
           left = (screen.width / 2) - (width / 2),
           top = (screen.height / 2) - (height / 2);
 
-      window.addEventListener("message", function(event) {
+      window.addEventListener("message", function (event) {
         var hash = JSON.parse(event.data);
         if (hash.type == 'access_token') {
           callback(hash.access_token);
@@ -36,10 +38,10 @@
         'Spotify',
         'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
        );
-          
+
     }; // end Login
 
-    user.getInfo = function(accessToken) {
+    user.getInfo = function (accessToken) {
       return $http({
         method: 'GET',
         url: mySpotifyUrl,
@@ -49,8 +51,7 @@
       }).then(function successCallback(response) {
           user.id = response.data.id;
         }, function errorCallback(response) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
+          console.log(response);
         });
     };
 
@@ -65,8 +66,7 @@
       }).then(function successCallback(response) {
           user.playlists = response.data.items;
         }, function errorCallback(response) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
+          console.log(response);
         });
     }
 
@@ -85,25 +85,22 @@
       }).then(function successCallback(response) {
           user.playlistID = response.data.id;
         }, function errorCallback(response) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
+          console.log(response);
         });
     };
 
     // Add Song
-    user.addSong = function(accessToken, playlist, song) {
+    user.addSong = function(accessToken, playlist, track) {
       return $http({
         method: 'POST',
-        url: spotifyUrl + '/' + user.id + '/playlists/' + playlist + '/tracks?uris=spotify%3Atrack%' + song,
+        url: spotifyUrl + '/' + user.id + '/playlists/' + playlist + '/tracks?uris=spotify%3Atrack%' + track,
         headers: {
           'Authorization': 'Bearer ' + accessToken,
           'Content-Type': 'application/json'
         }
       }).then(function successCallback(response) {
-          console.log(response);
         }, function errorCallback(response) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
+          console.log(response);
         });
     };
 
@@ -118,11 +115,11 @@
       // Todo - figure out why this doesn't work when field is empty
     };
 
-    song.addToPlaylist = function(song, user, playlist) {
+    song.addToPlaylist = function(track, user, playlist) {
       user.login(function(accessToken) {
         user.getInfo(accessToken).then(function() {
           user.getPlaylists(accessToken).then(function() {
-            playlistnames = [];
+            var playlistnames = [];
             // add playlist names to array
             user.playlists.forEach(function(item) {
               playlistnames.push(item.name);
@@ -130,16 +127,16 @@
             // If playlist exists, add song
             if (playlistnames.indexOf(playlist) >= 0) {
               var oldPlaylist = user.playlists[playlistnames.indexOf(playlist)].id;
-              user.addSong(accessToken, oldPlaylist, song);
+              user.addSong(accessToken, oldPlaylist, track);
             }
             // Otherwise create playlist, add song
             else {
               user.addPlaylist(accessToken, playlist).then(function() {
-                user.addSong(accessToken, user.playlistID, song);
+                user.addSong(accessToken, user.playlistID, track);
               });
             }
           });
-        })
+        });
       });
     };
 
